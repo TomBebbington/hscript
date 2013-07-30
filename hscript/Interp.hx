d9b5f04fc9ac458dbfdd5b98b0ebdd5e746a932c
Tom Bebbington <tophattedcoder@gmail.com>
2013-07-30 21:54:59 +0100

    Revert "Tests in external file"
    This reverts commit ca3c41980e8b353133a68f885eb391db8299b095.

--- hscript/Interp.hx
+++ hscript/Interp.hx
@@ -435,8 +435,7 @@ class Interp {
 	function makeIterator(v:Dynamic):Iterator<Dynamic> {
 		if(v.iterator != null) v = v.iterator();
 		var c = Type.getClass(v);
-		if(c != null)
-			trace(Type.getClassName(c));
+		
 		if(c == null && (v.hasNext == null || v.next == null)) throw Error.EInvalidIterator(v);
 		return v;
 	}
